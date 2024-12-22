1f6e8811262c2e6ac6750389bad92a627480dcc5
khalid-nur <khalidnur150@gmail.com>
2024-12-22 13:18:13 -0600

    initial commit

---
+++ src/main/java/com/elibrary/backend/ElibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elibrary.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elibrary/backend/ElibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elibrary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
