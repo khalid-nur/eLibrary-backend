6f8805480f657a6a70f1c4aa5d14a0acecc43c4e
khalid-nur <khalidnur150@gmail.com>
2025-07-05 22:47:37 -0500

    Add review api to check if user has reviewed a book

--- src/main/java/com/elibrary/backend/modules/review/service/ReviewService.java
+++ src/main/java/com/elibrary/backend/modules/review/service/ReviewService.java
@@ -12,10 +12,20 @@ public interface ReviewService {
     /**
      * Fetches a paginated list of reviews for a specific book id
      *
-     * @param bookId the id of the book to fetch reviews for
+     * @param bookId   the id of the book to fetch reviews for
      * @param pageable pagination information
      * @return paginated list of reviews for the specified book
      */
     Page<Review> getReviewsByBookId(Long bookId, Pageable pageable);
+
+    /**
+     * Checks if the user has submitted a review for a book
+     *
+     * @param userEmail the email address of the user
+     * @param bookId    the id of the book to check review status for
+     * @return true if the user has reviewed the book; false otherwise
+     */
+    boolean isBookReviewedByUser(String userEmail, Long bookId);
+
 
 }
